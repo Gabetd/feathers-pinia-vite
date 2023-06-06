c52ad358dcec03a171816c06509debc9ac7d3753
Gabetd <gabetd0904@gmail.com>
2023-06-06 16:27:51 -0500

    config components issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
-// import AutoImport from 'unplugin-auto-import/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import Layouts from 'vite-plugin-vue-layouts'
 import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
-// import { feathersPiniaAutoImport } from 'feathers-pinia'
+import { feathersPiniaAutoImport } from 'feathers-pinia'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -32,40 +32,40 @@ export default defineConfig({
     Layouts(),
 
     // https://github.com/antfu/unplugin-auto-import
-    // AutoImport({
-    //   imports: [
-    //     'vue',
-    //     'vue-router',
-    //     'vue-i18n',
-    //     'vue/macros',
-    //     '@vueuse/head',
-    //     '@vueuse/core',
-    //     feathersPiniaAutoImport,
-    //   ],
-    //   dts: 'src/auto-imports.d.ts',
-    //   dirs: ['src/composables', 'src/models', 'src/stores'],
-    //   vueTemplate: true,
-    // }),
-
-    Components({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ['vue'],
-      // allow auto import and register components used in markdown
-      include: [/\.vue$/, /\.vue\?vue/],
-      dts: 'src/components.d.ts',
-      types: [
-        {
-          from: 'vue-router',
-          names: ['RouterLink', 'RouterView'],
-        },
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        'vue/macros',
+        '@vueuse/head',
+        '@vueuse/core',
+        feathersPiniaAutoImport,
       ],
-      resolvers: [
-        IconsResolver(),
-        (name: string) => {
-          if (name.match(/^Daisy[A-Z]/)) return { name, from: 'daisy-ui-kit' }
-        },
-      ],
+      dts: 'src/auto-imports.d.ts',
+      dirs: ['src/composables', 'src/models', 'src/stores'],
+      vueTemplate: true,
     }),
+
+    // Components({
+    //   // allow auto load markdown components under `./src/components/`
+    //   extensions: ['vue'],
+    //   // allow auto import and register components used in markdown
+    //   include: [/\.vue$/, /\.vue\?vue/],
+    //   dts: 'src/components.d.ts',
+    //   types: [
+    //     {
+    //       from: 'vue-router',
+    //       names: ['RouterLink', 'RouterView'],
+    //     },
+    //   ],
+    //   resolvers: [
+    //     IconsResolver(),
+    //     (name: string) => {
+    //       if (name.match(/^Daisy[A-Z]/)) return { name, from: 'daisy-ui-kit' }
+    //     },
+    //   ],
+    // }),
 
     // // https://github.com/antfu/unplugin-icons#migrate-from-vite-plugin-icons
     Icons(),
