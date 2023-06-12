a0e37f751b3a82b3bce9855801b994523c90acb6
Gabetd <gabetd0904@gmail.com>
2023-06-12 12:53:24 -0500

    reminders nav bar

--- src/pages/app/reminders.vue
+++ src/pages/app/reminders.vue
@@ -4,9 +4,12 @@ meta:
 </route>
 
 <template>
-  <DaisyFlex col items-center class="h-full gap-2 bg-primary-focus/5">
+  <div class="app-layout h-full">
+  <AppNav />
+  <DaisyFlex col items-center class="h-full gap-2 bg-primary-focus/5 -mt-16 pt-16">
     <div class="mt-8" />
 
     <TaskList />
   </DaisyFlex>
+</div>
 </template>
